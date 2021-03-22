d0c0499f326f88bcaa5aa5e33892627b2494b33d
Bruno Becoski <brunobecoski@gmail.com>
2021-03-21 23:09:14 -0300

    challenge update

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
+// import { toast } from 'react-toastify';
 import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { Product, /* Stock */ } from '../types';
 
 interface CartProviderProps {
   children: ReactNode;
@@ -25,6 +25,8 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
     // const storagedCart = Buscar dados do localStorage
 
+    // const storagedCart = localStorage.getItem('@RocketShoes:cart');
+    
     // if (storagedCart) {
     //   return JSON.parse(storagedCart);
     // }
@@ -35,6 +37,38 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const addProduct = async (productId: number) => {
     try {
       // TODO
+
+      const product = await api.get(`/products/${productId}`)
+        .then(response => response.data);
+
+      if(cart.length === 0) {
+        setCart([
+          {
+            amount: 1,
+            id: product.id,
+            image: product.image,
+            price: product.price,
+            title: product.title,
+          },
+        ]);
+      } else {
+        cart.map(product => {
+          if(product.id === productId) {
+            console.log('amount product')
+            setCart([...cart, {
+              amount: product.amount ++,
+              id: product.id,
+              image: product.image,
+              price: product.price,
+              title: product.title,
+            }]);
+          } else {
+            console.log('add product')
+            setCart([...cart, product]);
+          }               
+        });
+      }
+      
     } catch {
       // TODO
     }
